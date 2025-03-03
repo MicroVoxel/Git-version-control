cc025880ece0e689650911ba83570c6ee2aef294
MicroVoxel <machenga.koko@gmail.com>
2025-03-03 20:49:52 +0700

    commit

---
+++ Assets/Button1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button1 : MonoBehaviour
+{
+   public void Hello()
+    {
+        Debug.Log("Hello My Teacher!!");
+    }
+}
